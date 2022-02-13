020a83e7d37f559d2f6570807106d9a4f307047b
Jesse Duffield <jessedduffield@gmail.com>
2022-02-14 10:18:03 +1100

    fix

--- pkg/gui/list_context_config.go
+++ pkg/gui/list_context_config.go
@@ -266,7 +266,7 @@ func (gui *Gui) submodulesListContext() *context.SubmodulesContext {
 func (gui *Gui) suggestionsListContext() *context.SuggestionsContext {
 	return context.NewSuggestionsContext(
 		func() []*types.Suggestion { return gui.State.Suggestions },
-		gui.Views.Files,
+		gui.Views.Suggestions,
 		func(startIdx int, length int) [][]string {
 			return presentation.GetSuggestionListDisplayStrings(gui.State.Suggestions)
 		},
